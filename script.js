be5430b457a67cb3ebdeec3f35ccc70e3bb78ab5
jackisace <38766065+jackisace@users.noreply.github.com>
2022-02-23 13:17:38 +0000

    initial

---
+++ script.js
@@ -0,0 +1,46 @@
+console.log("OK");
+
+let lakeLatLng = {lat: 52.64937, lng: -0.67724};
+function submit(){
+  type = "";
+  
+  var ele = document.getElementsByName("a");
+    for(var i=0;i<ele.length;i++){
+      if(ele[i].checked){
+        console.log(ele[i].value);
+        type += ele[i].value + ", ";
+      }
+      ele[i].checked = false;
+    }
+      
+  var ele = document.getElementsByName("b");
+  for(var i=0;i<ele.length;i++){
+      if(ele[i].checked){
+        console.log(ele[i].value);
+        type += ele[i].value
+      }
+      ele[i].checked = false;
+    }
+  navigator.geolocation.getCurrentPosition(dbAdd,err,{enableHighAccuracy: true});
+}
+
+
+function drift(){
+  
+  
+  var ele = document.getElementsByName("a");
+    for(var i=0;i<ele.length;i++){
+      ele[i].checked = false;
+    }
+      
+  var ele = document.getElementsByName("b");
+  for(var i=0;i<ele.length;i++){
+      ele[i].checked = false;
+    }
+  
+  type = "Drift";
+  navigator.geolocation.getCurrentPosition(dbAdd,err,{enableHighAccuracy: true});
+}
+
+
+//document.documentElement.requestFullscreen();
